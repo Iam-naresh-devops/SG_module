08c072063d195a9afd604d1b4330659a26e57439
naresh <Naresh@gmail.com>
2025-12-31 15:43:15 +0530

    SG group  ingress testing

--- main.tf
+++ main.tf
@@ -27,6 +27,6 @@ resource "aws_security_group_rule" "dynamic_rules" {
   from_port   = var.from_port
   to_port     = var.to_port
   protocol    = "tcp"
-  security_group_id = var.sg_name
+  security_group_id = aws_security_group.sg.id
   source_security_group_id = var.source_security_group_id
 }
